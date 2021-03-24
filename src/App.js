f5f5cf21d2c8b7caf838e70030b68166b8b35ddf
Angela Csomo <angela.csomo@gmail.com>
2021-03-24 12:53:17 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import './App.css'
+import React, {useState, useEffect} from 'react';
+import Searchbar from 'material-ui-search-bar';
+
 
 const App = () => {
 
+
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+  
+
+    fetch("./api/clients?search{}")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => setData(null))
+     // .finally(() => setIsLoading(true));
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="App">
-      Todo...
+    <h1>Veterinarian admin</h1>
+    <input></input>
+    {/*}
+    <Searchbar 
+    value={this.state.value}
+    onChange={(newValue) => this.setState ({ value: NewValue })}
+    onRequestSearch={ () => doSomethingWith(this.state.value)}
+    />
+  */}
+
+  
+    
     </div>
   )
 }
